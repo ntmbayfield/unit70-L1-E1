850c74ae57f2c7057aeab2532bb578db74510bba
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-07-18 22:01:47 -0700

    Initial commit.

---
+++ src/Wrapper.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CartHeader from './CartHeader'
+
+const Wrapper = () => {
+  return (
+    <div>
+      <CartHeader />
+    </div>
+  )
+}
+
+export default Wrapper;
