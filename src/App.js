86a03b5f5e269f526e2fcfa1321d9588282511c4
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-08-16 18:23:54 -0700

    Update CartItem and CartItems component to display list of items in cart.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,41 @@ import CartHeader from './CartHeader';
 import CartFooter from './CartFooter';
 import CartItems from './CartItems';
 
+const cartItemsList = [
+  { id: 1,
+    product: {
+      id: 40,
+      name: 'Mediocre Iron Watch',
+      priceInCents: 399
+    },
+    quantity: 1
+  },
+  { id: 2,
+    product: {
+      id: 41,
+      name: 'Heavy Duty Concrete Plate',
+      priceInCents: 499
+    },
+    quantity: 2
+  },
+  { id: 3,
+    product:
+      { id: 42,
+        name: 'Intelligent Paper Knife',
+        priceInCents: 1999
+      },
+    quantity: 1
+  }
+]
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <CartHeader />
-          <CartItems />
-        <CartFooter />
+          <CartItems cartItemsList={cartItemsList}/>
+        <CartFooter copyright="&copy;" year="2016"/>
       </div>
     );
   }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from 'react'
+import './App.css';
 
-const CartItem = ({cartitem}) => {
-  <li className="collection-item">
-    This cartitem has a name of {cartitem.name}
-  </li>
+function CartItem(props) {
+  return (
+    <div class="list-group-item">
+      <div class="row">
+        <div class="col-md-8">{props.cartItem.product.name}</div>
+        <div class="col-md-2">{props.cartItem.product.priceInCents}</div>
+        <div class="col-md-2">{props.cartItem.quantity}</div>
+      </div>
+    </div>
+  );
 }
 
 
